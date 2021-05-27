e8349f4fc8ec8c33526f6d813028b19d2f434df0
ashugowda <54936438+ashugowda@users.noreply.github.com>
2021-05-27 14:50:48 +0530

    Update benchmarks.py

--- benchmarks.py
+++ benchmarks.py
@@ -8,6 +8,41 @@ SUPPORT_DIMENSION = [2, 10, 20, 30, 50, 100]
 SUPPORT_DIMENSION_2 = [10, 20, 30, 50, 100]
 
 
+def C1(solution=None, name="Rotated Bent Cigar Function", f_shift_file="shift_data_1_D", f_matrix_file="M_1_D", bias=100):
+    problem_size = len(solution)
+    f_shift, f_matrix = check_problem_size(problem_size, f_shift_file, f_matrix_file)
+    shift_data = load_shift_data__(f_shift)
+    matrix = load_matrix_data__(f_matrix)
+    z = dot(solution - shift_data, matrix)
+    return f1_bent_cigar__(z) + bias
+
+
+def C2(solution=None, name="Rotated Discus Function", f_shift_file="shift_data_2_D", f_matrix_file="M_2_D", bias=200):
+    problem_size = len(solution)
+    f_shift, f_matrix = check_problem_size(problem_size, f_shift_file, f_matrix_file)
+    shift_data = load_shift_data__(f_shift)
+    matrix = load_matrix_data__(f_matrix)
+    z = dot(solution - shift_data, matrix)
+    return f2_discus__(z) + bias
+
+
+def C3(solution=None, name="Shifted and Rotated Weierstrass Function", f_shift_file="shift_data_3_D", f_matrix_file="M_3_D", bias=300):
+    problem_size = len(solution)
+    f_shift, f_matrix = check_problem_size(problem_size, f_shift_file, f_matrix_file)
+    shift_data = load_shift_data__(f_shift)
+    matrix = load_matrix_data__(f_matrix)
+    z = dot(0.5*(solution - shift_data) / 100, matrix)
+    return f3_weierstrass__(z) + bias
+
+
+def C4(solution=None, name="Shifted and Rotated Schwefel’s Function", f_shift_file="shift_data_4_D", f_matrix_file="M_4_D", bias=400):
+    problem_size = len(solution)
+    f_shift, f_matrix = check_problem_size(problem_size, f_shift_file, f_matrix_file)
+    shift_data = load_shift_data__(f_shift)
+    matrix = load_matrix_data__(f_matrix)
+    z = dot(1000 * (solution - shift_data) / 100, matrix)
+    return f4_modified_schwefel__(z) + bias
+
 
 def C5(solution=None, name="Shifted and Rotated Ackley’s Function", shift_data_file="shift_data_5.txt", bias=500):
     problem_size = len(solution)
@@ -23,6 +58,8 @@ def C5(solution=None, name="Shifted and Rotated Ackley’s Function", shift_data
     matrix = load_matrix_data__(f_matrix)
     z = dot(solution - shift_data, matrix)
     return f5_ackley__(z) + bias
+
+
 
 def C6(solution=None, name="Shifted Rastrigin’s Function", shift_data_file="shift_data_8.txt", bias=800):
     problem_size = len(solution)
@@ -40,6 +77,8 @@ def C6(solution=None, name="Shifted Rastrigin’s Function", shift_data_file="sh
     z = dot(z, matrix)
     return f8_rastrigin__(z) + bias
 
+
+
 def C7(solution=None, name="Shifted and Rotated Griewank’s Function", shift_data_file="shift_data_7.txt", bias=700):
     problem_size = len(solution)
     if problem_size > 100:
@@ -55,6 +94,28 @@ def C7(solution=None, name="Shifted and Rotated Griewank’s Function", shift_da
     z = 600 * (solution - shift_data) / 100
     z = dot(z, matrix)
     return f7_griewank__(z) + bias
+
+
+
+
+def C8(solution=None, name="Shifted and Rotated Expanded Griewank’s plus Rosenbrock’s Function",
+       f_shift_file="shift_data_8_D", f_matrix_file="M_8_D", bias=800):
+    problem_size = len(solution)
+    f_shift, f_matrix = check_problem_size(problem_size, f_shift_file, f_matrix_file)
+    shift_data = load_shift_data__(f_shift)
+    matrix = load_matrix_data__(f_matrix)
+    z = dot(5 * (solution - shift_data) / 100, matrix) + 1
+    return f8_expanded_griewank__(z) + bias
+
+
+def C9(solution=None, name="Shifted and Rotated Expanded Scaffer’s F6 Function",
+       f_shift_file="shift_data_9_D", f_matrix_file="M_9_D", bias=900):
+    problem_size = len(solution)
+    f_shift, f_matrix = check_problem_size(problem_size, f_shift_file, f_matrix_file)
+    shift_data = load_shift_data__(f_shift)
+    matrix = load_matrix_data__(f_matrix)
+    z = dot(solution - shift_data, matrix) + 1
+    return f9_expanded_scaffer__(z) + bias
 
 
 def C10(solution=None, name="Shifted Schwefel’s Function", shift_data_file="shift_data_10.txt", bias=1000):
@@ -88,6 +149,7 @@ def C11(solution=None, name="Shifted and Rotated Schwefel’s Function", shift_d
     z = 1000 * (solution - shift_data) / 100
     z = dot(z, matrix)
     return f9_modified_schwefel__(z) + bias
+
 
 def C12(solution=None, name="Shifted and Rotated Katsuura Function", shift_data_file="shift_data_12.txt", bias=1200):
     problem_size = len(solution)
@@ -135,6 +197,71 @@ def C14(solution=None, name="Shifted and Rotated Expanded Scaffer’s F6 Functio
     matrix = load_matrix_data__(f_matrix)
     z = dot(solution - shift_data, matrix) + 1
     return f14_expanded_scaffer__(z) + bias
+
+
+
+def C15(solution=None, name="Hybrid Function 1 (N=3)", f_shift_file="shift_data_10_D", f_matrix_file="M_10_D",
+        f_shuffle_file="shuffle_data_10_D", bias=1000):
+    problem_size = len(solution)
+    p = array([0.3, 0.3, 0.4])
+    n1 = int(ceil(p[0] * problem_size))
+    n2 = int(ceil(p[1] * problem_size))
+    f_shift, f_matrix, f_shuffle = check_problem_size(problem_size, f_shift_file, f_matrix_file, f_shuffle_file)
+    shift_data = load_shift_data__(f_shift)
+    matrix = load_matrix_data__(f_matrix)
+    shuffle = (load_shift_data__(f_shuffle) - ones(problem_size)).astype(int)
+    idx1 = shuffle[:n1]
+    idx2 = shuffle[n1:(n1 + n2)]
+    idx3 = shuffle[(n1 + n2):]
+    mz = dot(solution - shift_data, matrix)
+    return f4_modified_schwefel__(mz[idx1]) + f12_rastrigin__(mz[idx2]) + f13_elliptic__(mz[idx3]) + bias
+
+
+
+def C16(solution=None, name="Hybrid Function 2 (N=4)", f_shift_file="shift_data_11_D", f_matrix_file="M_11_D",
+        f_shuffle_file="shuffle_data_11_D", bias=1100):
+    problem_size = len(solution)
+    p = array([0.2, 0.2, 0.3, 0.3])
+    n1 = int(ceil(p[0] * problem_size))
+    n2 = int(ceil(p[1] * problem_size))
+    n3 = int(ceil(p[2] * problem_size))
+
+    f_shift, f_matrix, f_shuffle = check_problem_size(problem_size, f_shift_file, f_matrix_file, f_shuffle_file)
+    shift_data = load_shift_data__(f_shift)
+    matrix = load_matrix_data__(f_matrix)
+    shuffle = (load_shift_data__(f_shuffle) - ones(problem_size)).astype(int)
+    idx1 = shuffle[:n1]
+    idx2 = shuffle[n1:(n1 + n2)]
+    idx3 = shuffle[(n1 + n2):(n1+n2+n3)]
+    idx4 = shuffle[(n1 + n2 + n3):]
+    mz = dot(solution - shift_data, matrix)
+    return f11_griewank__(mz[idx1]) + f3_weierstrass__(mz[idx2]) + f10_rosenbrock__(mz[idx3]) + f9_expanded_scaffer__(mz[idx4]) + bias
+
+
+def C17(solution=None, name="Hybrid Function 3 (N=5)", f_shift_file="shift_data_12_D", f_matrix_file="M_12_D",
+        f_shuffle_file="shuffle_data_12_D", bias=1200):
+    problem_size = len(solution)
+    p = array([0.1, 0.2, 0.2, 0.2, 0.3])
+    n1 = int(ceil(p[0] * problem_size))
+    n2 = int(ceil(p[1] * problem_size))
+    n3 = int(ceil(p[2] * problem_size))
+    n4 = int(ceil(p[3] * problem_size))
+
+    f_shift, f_matrix, f_shuffle = check_problem_size(problem_size, f_shift_file, f_matrix_file, f_shuffle_file)
+    shift_data = load_shift_data__(f_shift)
+    matrix = load_matrix_data__(f_matrix)
+    shuffle = (load_shift_data__(f_shuffle) - ones(problem_size)).astype(int)
+    idx1 = shuffle[:n1]
+    idx2 = shuffle[n1:(n1 + n2)]
+    idx3 = shuffle[(n1 + n2):(n1 + n2 + n3)]
+    idx4 = shuffle[(n1 + n2 + n3):(n1+n2+n3+n4)]
+    idx5 = shuffle[(n1+n2+n3+n4):]
+    mz = dot(solution - shift_data, matrix)
+    return f5_katsuura__(mz[idx1]) + f6_happy_cat__(mz[idx2]) + f8_expanded_griewank__(mz[idx3]) + f4_modified_schwefel__(mz[idx4]) + \
+           f14_ackley__(mz[idx5]) + bias
+
+
+
 
 
 
